f6cf981a4594ef05ee73c4bbfd0379ef9a3ef9a2
chuyukeji <33227605+chuyukeji@users.noreply.github.com>
2018-08-04 16:04:34 +0800

    Update HttpUtil.java

--- app/src/main/java/com/coolweather/android/util/HttpUtil.java
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -8,7 +8,7 @@ public class HttpUtil {
     public static void sendOkHttpRequest(String address, okhttp3.Callback callback) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(address).build();
-        client.newCall(request).enqueue(callback);
+        client.newCall(request).enqueue(callback);//发送请求，回调okhttp3.Callback接口，然后调用接口里面重写的两个方法，一个onResponse，一个onFailer
     }
 
 }
